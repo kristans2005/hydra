b32ca3ea42531ecc0b6ee0f0845b857416b11c79
MarkussIPA22 <markusspetrovs0@gmail.com>
2024-10-22 08:54:04 +0300

    kkas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="bg-gray-900 text-white min-h-screen py-12">
+    <div class="bg-gray-900 text-white min-h-screen py-12 overflow-hidden">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-gray-800 overflow-hidden shadow-lg sm:rounded-lg">
                 <div class="p-6 text-yellow-500">
@@ -16,12 +16,13 @@
         </div>
     </div>
 
-    <nav class="bg-gray-900 py-4 shadow-md">
+    <nav class="bg-gray-900 py-4 shadow-md overflow-hidden">
         <div class="container mx-auto px-4 flex justify-between items-center">
             <div class="flex items-center">
-                <a href="#" class="text-2xl font-bold text-yellow-500">Casino Royale</a>
+              
             </div>
             <div class="flex items-center space-x-4">
+           
             </div>
         </div>
     </nav>
